394f510622dc03d08b9eb1c1b96420c2f3d87bd4
Adrian <adrianrotila@gmail.com>
2020-11-15 11:27:12 +0200

    Created all repositoryes

---
+++ onlinestore/src/main/java/com/sda/onlinestore/repository/ProductRepository.java
@@ -0,0 +1,10 @@
+package com.sda.onlinestore.repository;
+
+
+import com.sda.onlinestore.entity.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+interface ProductRepository extends JpaRepository<Product, Long> {
+}
---
+++ onlinestore/src/main/java/com/sda/onlinestore/repository/UserAccountRepository.java
@@ -0,0 +1,9 @@
+package com.sda.onlinestore.repository;
+
+import com.sda.onlinestore.entity.UserAccount;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserAccountRepository extends JpaRepository<UserAccount, Long> {
+}
