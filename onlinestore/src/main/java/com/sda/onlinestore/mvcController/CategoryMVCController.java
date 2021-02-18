57a44e79d016dcbbb273e1a6aa3f78b94e0eb922
Adrian <adrianrotila@gmail.com>
2021-02-18 19:10:28 +0000

    no message

--- onlinestore/src/main/java/com/sda/onlinestore/mvcController/CategoryMVCController.java
+++ onlinestore/src/main/java/com/sda/onlinestore/mvcController/CategoryMVCController.java
@@ -1,4 +1,17 @@
 package com.sda.onlinestore.mvcController;
 
+import com.sda.onlinestore.service.CategoryService;
+import com.sda.onlinestore.service.UserAccountService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+
+@Controller
 public class CategoryMVCController {
+
+    private final CategoryService categoryService;
+
+    @Autowired
+    public CategoryMVCController(CategoryService categoryService){
+        this.categoryService = categoryService;
+    }
 }
--- onlinestore/src/main/java/com/sda/onlinestore/mvcController/RoleMVCController.java
+++ onlinestore/src/main/java/com/sda/onlinestore/mvcController/RoleMVCController.java
@@ -1,10 +1,20 @@
 package com.sda.onlinestore.mvcController;
 
+import com.sda.onlinestore.service.RoleService;
+import com.sda.onlinestore.service.UserAccountService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class RoleMVCController {
+
+    private final RoleService roleService;
+
+    @Autowired
+    public RoleMVCController(RoleService roleService) {
+        this.roleService = roleService;
+    }
 
     @GetMapping("/viewRole")
     public String viewRole(){
